273b2e57dbec407d2c880c36817790c7c8a4514f
Fabrizio <juniorhuamanro@gmail.com>
2023-02-28 08:41:01 +0100

    version4

--- game.js
+++ game.js
@@ -1,5 +1,7 @@
 let player = 1
 const casilla = [[0, 0, 0], [0,0,0], [0,0,0]]
+let iRand = getRandomInt(3)
+let jRand = getRandomInt(3)
 
 const desactivarCasillas= () => {
     for (let i=0; i < 3; i++) {
@@ -8,6 +10,10 @@ const desactivarCasillas= () => {
         }
     }
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
 
 const hayEmpate = () => {
     for (let i=0; i < 3; i++) {
@@ -18,61 +24,87 @@ const hayEmpate = () => {
     return true
 }
 
+const resultado = () =>{
+    if (player = 1) {
+        document.getElementById("resultado").innerText = "Bueno ganaste como siempre uwu"
+    } else{
+        document.getElementById("resultado").innerText = "Cómo fue que gané? xd"
+    }
+}
+
 const ganador = () =>{
     if (casilla[0][0] != 0 && casilla[0][0]==casilla[0][1] && casilla[0][1]==casilla[0][2]) {
-        console.log(`GANADOR ES EL JUGADOR ${player}`)
+        resultado()
         desactivarCasillas()
     }
     if (casilla[1][0] != 0 && casilla[1][0]==casilla[1][1] && casilla[1][1]==casilla[1][2]) {
-        console.log(`GANADOR ES EL JUGADOR ${player}`)
+        resultado()
         desactivarCasillas()
     }
     if (casilla[2][0] != 0 && casilla[2][0]==casilla[2][1] && casilla[2][1]==casilla[2][2]) {
-        console.log(`GANADOR ES EL JUGADOR ${player}`)
+        resultado()
         desactivarCasillas()
     }
     if (casilla[0][0] != 0 && casilla[0][0]==casilla[1][0] && casilla[1][0]==casilla[2][0]) {
-        console.log(`GANADOR ES EL JUGADOR ${player}`)
+        resultado()
         desactivarCasillas()
     }
     if (casilla[0][1] != 0 && casilla[0][1]==casilla[1][1] && casilla[1][1]==casilla[2][1]) {
-        console.log(`GANADOR ES EL JUGADOR ${player}`)
+        resultado()
         desactivarCasillas()
     }
     if (casilla[0][2] != 0 && casilla[0][2]==casilla[1][2] && casilla[1][2]==casilla[2][2]) {
-        console.log(`GANADOR ES EL JUGADOR ${player}`)
+        resultado()
         desactivarCasillas()
     }
     if (casilla[0][0] != 0 && casilla[0][0]==casilla[1][1] && casilla[1][1]==casilla[2][2]) {
-        console.log(`GANADOR ES EL JUGADOR ${player}`)
+        resultado()
         desactivarCasillas()
     }
     if (casilla[0][2] != 0 && casilla[0][2]==casilla[1][1] && casilla[1][1]==casilla[2][0]) {
-        console.log(`GANADOR ES EL JUGADOR ${player}`)
+        resultado()
         desactivarCasillas()
     }
     if (hayEmpate()) {
         desactivarCasillas()
-        document.getElementById("titulo").innerText = "Empate"
+        document.getElementById("resultado").innerText = "Empatamos bb, simplemente conectados"
     }
 }
+
+const fabri = (fila, col) =>{
+    
+    while (casilla[fila][col]==0 || casilla[fila][col] != 1) {
+        iRand = getRandomInt(3)
+        jRand = getRandomInt(3)
+        document.getElementById(`${iRand}-${jRand}`).innerText = "O"
+        
+        console.log("se convierte en player 1")
+        break        
+    }
+    ganador()
+    player = 1
+    
+}
+
 const gameOn = (fila,col)=>{
  
     if (casilla[fila][col]==0) {
         casilla[fila][col] = player
     
         if(player == 1){
-            document.getElementById(`${fila}-${col}`).innerText = "O"
-            ganador()
-            player = 2
-            console.log("se convierte en player 1")
-    }
-        else{
             document.getElementById(`${fila}-${col}`).innerText = "P"            
             ganador()
-            player = 1
-            console.log("se convierte en player 0")
-        }
+            player = 2
+            console.log("se convierte en player 2")
+    }
+        else{
+            
+                document.getElementById(`${fila}-${col}`).innerText = "O"
+                ganador()
+                player = 1
+                console.log("se convierte en player 1")       
+            }
+    
     }
 }
 
